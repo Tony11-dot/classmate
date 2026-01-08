6191835c0e8fc32615c2d62b49be68d70978973f
Tony <aboudtony22@gmail.com>
2026-01-08 19:07:20 +0200

    Tests: fix default root e2e expectation

--- services/api/test/app.e2e-spec.ts
+++ services/api/test/app.e2e-spec.ts
@@ -16,10 +16,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET) -> 404 (no root route)', () => {
+    return request(app.getHttpServer()).get('/').expect(404);
   });
 });
