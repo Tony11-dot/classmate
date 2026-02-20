4ee15ff38d53b5eec9dce18328225ccb5e485698
Tony <aboudtony22@gmail.com>
2026-02-20 22:53:21 +0200

    test(e2e): make jest-e2e.setup.cjs a valid empty setup file

--- services/api/test/jest-e2e.setup.cjs
+++ services/api/test/jest-e2e.setup.cjs
@@ -1,8 +1,2 @@
-
-
-beforeAll(() => {
-  
-    stdio: "inherit",
-    env: process.env,
-  });
-});
+// Intentionally empty.
+// DB schema push is handled once in globalSetup (jest-e2e.global-setup.cjs).
