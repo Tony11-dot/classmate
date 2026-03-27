5b64c6030ba15f852a24c1a9145280486c09ab4b
Tony <aboudtony22@gmail.com>
2026-03-27 20:39:06 +0300

    restore dm message kind migration file

--- services/api/prisma/migrations/20260321_dm_message_kind_expand/migration.sql
+++ services/api/prisma/migrations/20260321_dm_message_kind_expand/migration.sql
@@ -1,2 +1,18 @@
-ALTER TYPE "DmMessageKind" ADD VALUE IF NOT EXISTS 'VIDEO';
-ALTER TYPE "DmMessageKind" ADD VALUE IF NOT EXISTS 'FILE';
+-- dm message kind expand
+DO $$ BEGIN
+  ALTER TYPE "DmMessageKind" ADD VALUE 'VOICE';
+EXCEPTION
+  WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+  ALTER TYPE "DmMessageKind" ADD VALUE 'VIDEO';
+EXCEPTION
+  WHEN duplicate_object THEN null;
+END $$;
+
+DO $$ BEGIN
+  ALTER TYPE "DmMessageKind" ADD VALUE 'FILE';
+EXCEPTION
+  WHEN duplicate_object THEN null;
+END $$;
